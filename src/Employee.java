2aa7e2442bcc5c4cc2df9fc2f3b543c96d31e0a1
Alex <gt-star@yandex.ru>
2022-08-11 23:44:44 +0700

    Сделал поиск по мин и макс зп, подсчет всех затрат выплат по зп

---
+++ src/Employee.java
@@ -0,0 +1,87 @@
+import java.util.Objects;
+
+public class Employee {
+
+    private String lastName;
+    private String firstName;
+    private String mediumName;
+    private int department;
+    private double salary;
+    private int id;
+    private static int count = 1;
+    public Employee(String lastName, String firstName, String mediumName, int department, double salary) {
+        this.firstName = firstName;
+        this.mediumName = mediumName;
+        this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMediumName() {
+        return mediumName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудники:\n" +
+                "ID = '" + id + '\'' +
+                "Фамилия ='" + lastName + '\'' +
+                ",Имя ='" + firstName + '\'' +
+                ",Отчество='" + mediumName + '\'' +
+                ",Отдел=" + department +
+                ",зарплата =" + salary;
+    }
+    public double sumSalary() {
+        double sum = 0;
+        sum = salary + sum;
+        return sum;
+    }
+    public double minSalaryEmployee(){
+        double minSum = Double.MAX_VALUE;;
+        minSum = Math.min(minSum, salary);
+        return  minSum;
+    }
+    public double maxSalaryEmployee(){
+        double maxSum = Double.MIN_VALUE;;
+        maxSum = Math.max(maxSum, salary);
+        return  maxSum;
+    }
+//    public static double mediumSalary(){
+//
+//    }
+//    public static String printEmployee(){
+//
+//    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee[] employee = new Employee[10];
+        employee[0]= new Employee("Иванов", "Иван", "Иванович", 1, 520000);
+        employee[1]= new Employee("Иванов", "Иван", "Иванович", 2, 45000);
+        employee[2]= new Employee("Иванов", "Иван", "Иванович", 3, 40000);
+        employee[3]= new Employee("Иванов", "Иван", "Иванович", 4, 35000);
+        employee[4]= new Employee("Иванов", "Иван", "Иванович", 5, 30000);
+        employee[5]= new Employee("Иванов", "Иван", "Иванович", 1, 50000);
+        employee[6]= new Employee("Иванов", "Иван", "Иванович", 2, 1111000);
+        employee[7]= new Employee("Иванов", "Иван", "Иванович", 3, 40000);
+        employee[8]= new Employee("Иванов", "Иван", "Иванович", 4, 35000);
+        employee[9]= new Employee("Иванов", "Иван", "Иванович", 5, 30000);
+
+        double sum = 0;
+        for (int i = 0; i < employee.length;i++){
+            sum = employee[i].sumSalary() + sum;
+        }
+        System.out.println(sum);
+
+        double minSum = 0;
+        for (int i = 0; i < employee.length;i++){
+            minSum = employee[i].minSalaryEmployee();
+        }
+        System.out.println(minSum);
+
+        double maxSum = 0;
+        for (int i = 0; i < employee.length;i++){
+            if (maxSum < employee[i].maxSalaryEmployee()){
+                maxSum = employee[i].maxSalaryEmployee();
+            }
+        }
+        System.out.println(maxSum);
+
+
     }
 }
